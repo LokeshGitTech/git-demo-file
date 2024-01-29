004e9d02f58779a28bc509472b5d3eb3ca6ed849
LokeshGitTech <lokeshpatil11022000@gmail.com>
2024-01-29 11:59:33 +0530

    add react-router-dom and other files

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Demo1 from './demo1';
+import Demo2 from './demo2';
 
 function App() {
   return (
     <div className="App">
      <h1>Main branch code</h1>
+     <Demo1 />
+     <Demo2 />
     </div>
   );
 }
